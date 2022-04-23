94f33cecb3ef8ea15283a6bdbcf6d18b683317b6
Aaron <rhanfwlq980@naver.com>
2022-04-23 21:29:11 +0900

    수정

--- Makefile
+++ Makefile
@@ -1,6 +1,15 @@
-server : server.o
-	gcc -o server server.o
-server.o : server.c
-		gcc -c -o server.o server.c
-# clean :
-# 		rm *.o ex
+CC=gcc
+CFLAGS=-g -Wall
+OBJS=server.o
+TARGET=server
+
+$(TARGET): $(OBJS)
+	$(CC) -o $@ $(OBJS)
+
+server.o: server.c
+	gcc -c server.c
+
+clean:
+	rm -f *.o
+	rm -f *.out
+	rm -f $(TARGET)
--- server.c
+++ server.c
@@ -19,10 +19,8 @@ int main(int argc, char *argv[]) {
     int sockfd, newsockfd;
     int portno;
     socklen_t clilen;
-    FILE* hfile;
-    char buffer[25555], header[65535];
+    char buffer[10000], header[10000];
     memset(header, 0, sizeof(header));
-    pid_t pid;
 
     struct sockaddr_in serv_addr, cli_addr; // 소켓 통신을 위한 구조체 선언
 
@@ -38,7 +36,8 @@ int main(int argc, char *argv[]) {
     }
 
 
-    bzero((char *)&serv_addr, sizeof(serv_addr));  // 서버 주소를 초기화
+    memset((char *)&serv_addr, 0, sizeof(serv_addr));
+    // bzero((char *)&serv_addr, sizeof(serv_addr));  // 서버 주소를 초기화
     portno = atoi(argv[1]);                        // 포트 번호 변환
     serv_addr.sin_family = AF_INET;                // IPv4 프로토콜
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -63,40 +62,38 @@ int main(int argc, char *argv[]) {
     int fd;
 
     while (1) {
+        memset(buffer, 0, sizeof(buffer));
+        memset(data, 0x00, sizeof(data));
+        memset(header, 0x00, sizeof(header));
         // request를 받음
         newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
 
         // client로부터 올 msg를 buffer에 담기 위해 초기화
-        bzero(buffer, 25555);
         // socket으로부터 읽어서 buffer에 채워준다.
-        n = read(newsockfd, buffer, 25555);
+        n = read(newsockfd, buffer, 10000);
         // 확인을 위한 프린트
         printf("%s\n", buffer);
         // 요청한 request msg에서 index를 파악하기 위해 사용
         char *tok = strtok(buffer, " ");
         tok = strtok(NULL, " ");
-        // 반복문을 돌 때마다 data와 header를 새로 초기화
-        memset(data, 0x00, sizeof(data));
-        memset(header, 0x00, sizeof(header));
-
 
         if (strcmp(tok, "/index.html") == 0) {
-
-            // Header
             snprintf(header, sizeof(header), 
             "HTTP/1.0 200 OK\r\n"
-            "Content-Length: 2555\r\n"
+            "Content-Length: 10000\r\n"
             "Content-Type: text/html\r\n"
             "\r\n");
 
-            // index 일 때의 HTML Data
             snprintf(data, sizeof(data),
                 "<!DOCTYPE html>\n"
                 "<html>\n"
                 "<head>\n"
+                    "<meta charset='utf-8'>\n"
                 "</head>\n"
                 "<body>\n"
                     "<h1>[CSE3027] Computer Networks</h1>\n"
+                    "<h2>2018044411</h2>\n"
+                    "<h2>소프트웨어학부 김승호</h2>\n"
                     "<a href=\"./jpeg\">JPEG</a><br/>\n"
                     "<a href=\"./gif\">GIF</a><br/>\n"
                     "<a href=\"./mp3\">MUSIC</a><br/>\n"
@@ -108,18 +105,16 @@ int main(int argc, char *argv[]) {
 
         else if (strcmp(tok, "/jpeg") == 0) {
             int fd = open("./cat-min.jpeg", O_RDWR);
-            // 파일 크기를 len에 저장
             len = lseek(fd, 0, SEEK_END);
-            // 파일 디스크립터 포인터를 다시 원래 자리로 돌려놓는다.
             lseek(fd, 0, SEEK_SET);
-            // 파일로부터 읽은 데이터를 data 버퍼에 채워준다.
             read(fd, data, sizeof(data) - 1);
 
             snprintf(header, sizeof(header),
                 "HTTP/1.0 200 OK\r\n"
-                "Accept-Ranges: bytes\r\n"
+                // "Accept-Ranges: bytes\r\n"
                 "Content-Length: %ld\r\n"
                 "Content-Type: image/jpeg\r\n"
+                "Connetection: keep-alive\r\n"
                 "\r\n"
             , len); // Header 작성 : Content-Length는 보내줄 파일의 크기만큼 설정
         }
@@ -164,6 +159,19 @@ int main(int argc, char *argv[]) {
                 "Content-Length: %ld\r\n"
                 "Content-Type: audio/mpeg\r\n"
                 "Connection: Keep-Alive\r\n"
+                "\r\n"
+            , len);
+        }
+        else if (strcmp(tok, "/favicon.ico") == 0) {
+            int fd = open("./favicon.ico", O_RDWR);
+            len = lseek(fd, 0, SEEK_END);
+            lseek(fd, 0, SEEK_SET);
+            read(fd, data, sizeof(data) - 1);
+
+            snprintf(header, sizeof(header),
+                "HTTP/1.0 200 OK\r\n"
+                "Content-Length: %ld\r\n"
+                "Content-Type: image/x-icon\r\n"
                 "\r\n"
             , len);
         }
